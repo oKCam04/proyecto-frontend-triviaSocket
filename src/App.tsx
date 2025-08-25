d5500c3f88e38466060002f3e0f3b3eb32bbb67a
Camilo <ch385593@gmail.com>
2025-08-25 11:46:02 -0500

    parte inicial frontend con tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom"
+
+
+function App() {
+
+  return (
+    <>
+      <Router>
+          <Routes>
+              <Route path='/' />
+          </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
